2abe92301a4978453713509143fe8123dc16eb96
ahmedraza01635 <ahmedraza3797863@gmail.com>
2026-05-22 02:24:58 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// src/pages/Home.jsx
+
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedProducts from "../components/FeaturedProducts";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturedProducts />
+    </>
+  );
+};
+
+export default Home;
