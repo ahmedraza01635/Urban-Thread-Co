f2a1d6cbd10583782896f920ff4cfcc5fa02eba6
ahmedraza01635 <ahmedraza3797863@gmail.com>
2026-06-02 21:50:57 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeaturedProducts from "../components/FeaturedProducts";
+import Collections from "../components/Collections";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Collections />
       <FeaturedProducts />
     </>
   );
