8b55ea538f5f08302f8fae5f3092c1ea5ebd2c12
ahmedraza01635 <ahmedraza3797863@gmail.com>
2026-06-03 23:41:17 +0500

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -68,6 +68,9 @@ const Cart = () => {
                                         <h2 className="text-2xl font-bold">
                                             {item.title}
                                         </h2>
+                                        <p className="text-sm text-gray-500">
+                                            Color: {item.selectedColor}
+                                        </p>
 
                                         <p className="text-gray-500 mt-2">
                                             ${item.price}
